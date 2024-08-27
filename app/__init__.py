0589aa2334e0ca2ca20291b5e8bdeeea7b4d63f0
rafaelgsousa <elderrafaelgomes@gmail.com>
2024-08-27 00:31:45 -0300

    refactor code

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-from flask_restful import Api
 
 from .connection_db import init_db
 
@@ -7,11 +6,10 @@ from .connection_db import init_db
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
-    api = Api(app)
     init_db(app)
 
     # Importa e registra as rotas
-    from .routes import initialize_routes
+    from .controller import initialize_routes
     initialize_routes(app)
 
     return app
--- app/connection_db.py
+++ app/connection_db.py
@@ -1,20 +1,3 @@
-# from mongoengine import connect
-# from mongoengine.errors import MongoEngineException
-
-
-# def connection():
-
-#     try:
-#         connect(
-#             db='DevOps',
-#             host='mongodb',
-#             port=27017,
-#             username='devops',
-#             password='devops007'
-#             )
-#     except MongoEngineException as exc:
-#         print(f'Error={exc}')
-
 from flask_mongoengine import MongoEngine
 
 db = MongoEngine()
